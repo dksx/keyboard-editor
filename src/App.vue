2574acedee9a516113db2f4059a0b6f04529e8e8
dksx <37441849+dksx@users.noreply.github.com>
2021-10-22 15:40:09 +0200

    init repo

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-main>
+      <Keeb/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Keeb from './components/Keeb2';
+
+export default {
+  name: 'App',
+
+  components: {
+    Keeb,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style lang="scss">
+
+</style>
